941d4dca47cb6ae66715293dce375148f3ab5a54
Gadi Oren <gadi@lucidel.com>
2014-02-21 10:45:06 -0500

    Setting the static paths

--- moma_example/moma_example/settings.py
+++ moma_example/moma_example/settings.py
@@ -79,18 +79,17 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = rel('..', 'static')
+
+STATICFILES_DIRS = (
+    rel('static'),
+    rel('..', 'static'),
+)
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
