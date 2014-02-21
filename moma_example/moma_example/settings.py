2abd2815c204c0915262b4595761a8ec17c60447
Gadi Oren <gadi@lucidel.com>
2014-02-21 10:47:03 -0500

    Setting the static paths

--- moma_example/moma_example/settings.py
+++ moma_example/moma_example/settings.py
@@ -35,7 +35,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/ubuntu/project/moma-django/moma_example/db.sqlite',  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
